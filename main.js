ab0ade31c7ed97c95bb7f34dbab4b49f5a332ce8
vinolamusic <vinodream100@gmail.com>
2024-08-22 08:45:17 -0700

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-// Example JavaScript functionality
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Music website is ready!");
-    
-    // Add any custom JS here
-});
